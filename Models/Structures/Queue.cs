e57afdd5da2b293040ef271cc1b8fb8b42164de6
KovalNikitok <52690668+KovalNikitok@users.noreply.github.com>
2022-08-29 12:59:04 +0600

    Added queue data structure

---
+++ Models/Structures/Queue.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+
+namespace DataStructures.Models
+{
+    class Queue<T> : IEnumerable
+    {
+        private List<T> _queue;
+        private T Head => _queue[Count - 1];
+        private T Tail => _queue[0];
+        public int Count => _queue.Count;
+        public Queue()
+        {
+            _queue = new List<T>();
+        }
+        public Queue(T data) : this()
+        {
+            _queue.Add(data);
+        }
+
+        public void Enqeue(T data)
+        {
+            _queue.Insert(0, data);
+        }
+
+        public T Deqeue()
+        {
+            if (Count <= 0)
+                throw new NullReferenceException("Queue have not elements.");
+            var item = Head;
+            _queue.RemoveAt(Count - 1);
+            return item;
+        }
+        public T Peek()
+        {
+            if (Count <= 0)
+                throw new NullReferenceException("Queue have not elements.");
+            return Head;
+        }
+
+        public T PeekBack()
+        {
+            if (Count <= 0)
+                throw new NullReferenceException("Queue have not elements.");
+            return Tail;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            if (Count <= 0)
+                yield break;
+
+            for(int i = Count; i > 0; i--)
+            {
+                yield return _queue[i - 1];
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace DataStructures
             var lStack = new LinkedStack<int>();
             var biList = new BidirectionalList<int>();
             var circList = new CircularList<int>();
+            var queue = new Queue<int>();
 
             for (int i = 0; i < 5; i++)
             {
@@ -18,6 +19,7 @@ namespace DataStructures
                 list.Add(i + 7);
                 biList.Add(i * 10);
                 circList.Add(14 * i);
+                queue.Enqeue(21 - i * 2);
             }
             
 
@@ -51,6 +53,19 @@ namespace DataStructures
             circList.Delete(28);
             Console.WriteLine($"Circular list has {circList.Count} elements");
             foreach (var item in circList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            
+            Console.WriteLine($"Queue has {queue.Count} elements");
+            Console.WriteLine(queue.PeekBack());
+            Console.WriteLine();
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine();
+            Console.WriteLine(queue.Deqeue());
+            foreach (var item in queue)
             {
                 Console.WriteLine(item);
             }
